820b154559104e54f8cdc77991962b0fb27ab0b0
spoiledragon <karlo.gallegos2031@alumnos.udg.mx>
2020-09-28 10:18:49 -0500

    se añadieron los valores y da el resultado

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    unsigned int a,b;
 
+    cout << "area:" <<endl;
+    cin >> a;
+    cout << "base:" <<endl;
+    cin >> b;
+    cout << (a*b)/2<<endl;
+    system("PAUSE"); 
     return 0;
 }
